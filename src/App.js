4575bd6ba7843cf77d8b0f9c3865b46a6200413f
Imran Hossain <imranhossainemon1717@gmail.com>
2022-11-25 20:58:35 +0600

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>wELLCOME TO MY PHONE GALLERY</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+}
+
+export default App;
